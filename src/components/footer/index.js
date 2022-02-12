32f6d5e090ad06dab532556bac9901b66e124583
krod2016 <katrinajrodriguez1993@gmail.com>
2022-02-11 22:41:44 -0600

    updated footer, and nav

---
+++ src/components/footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Footer() {
+
+
+    return (
+        <footer>
+            <div className="my-3">
+                <a href="https://github.com/krod2016">Github</a>
+                <a href="https://www.linkedin.com/in/katrina-rodriguez-0b623b218/">Linkedin</a> 
+                <a href="https://twitter.com/k_rodriguez210">Twitter</a>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
