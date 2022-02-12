b2c33762751a8581c84d31dbcbbe36f60ab50bb3
krod2016 <katrinajrodriguez1993@gmail.com>
2022-02-11 20:07:20 -0600

    updated about section

---
+++ src/components/about/index.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import coverImage from '../assets/images/katrina.jpg';
+function About() {
+  return (
+    <section className="my-5">
+      <h1 id="about">About Me</h1>
+      <img src={coverImage} className="my-2" style={{ width: "30%" }} alt="cover" />
+      <div className="my-2">
+        <p>
+        Hello! Thank you for reading my portfolio. My name is Katrina Rodriguez, and I am from San Antonio, Texas. I am currently on the tail end of my Full Stack Developer Certificate from UT-Austin. I have a bachelors of Science from Utah State University (Go Aggies!) in Anthropology, and a certificate in Museum Studies. I love to learn about everything, and anything that I can get my hands on.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
